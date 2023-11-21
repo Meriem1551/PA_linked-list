a5d7735871603d57f0258f98ba1dec6b7acb3483
Meriem1551 <meriem200409@gmail.com>
2023-11-21 19:30:59 +0100

    Creating an empty list

---
+++ part_1/emptyList.c
@@ -0,0 +1,11 @@
+#include "simplyLL.h"
+/**
+ * cree_list_vide - create an empty list
+ * Return: pointer to empty list
+ */
+
+Element *cree_liste_vide()
+{
+    Element *h = NULL;
+    return (h);
+}
